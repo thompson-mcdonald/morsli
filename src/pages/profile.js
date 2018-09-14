cb8bc1961960f7cd0461c99226cc02446fdbf979
jacob thompson <thampsen@gmail.com>
2018-09-14 10:59:56 +0100

    include dimensions

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -6,7 +6,7 @@ import Container from '../components/container'
 const Profile = () => (
   <div>
     <Container classNames='profile'>
-      <p>A 22-Year old video editor based in London, UK. Having just finished University, I’m experienced in making video content across the spectrum, from commercial social-media to experimental films. I’m currently working full-time creating branded content and social-media materials for Kyra TV, producing high-quality promotional videos and bringing new audience awareness to the YouTube platform. In my free time, I enjoy producing self-directed experimental videos alongside an avid interest in music and photography, leading to me shooting live events over the past few years -  including large festivals such as Sunfall (London) and Soundwave (Croatia). </p>
+      <p>A 22-Year old video editor based in London, UK. Having just finished University, I’m experienced in making video content across the spectrum, from commercial social-media to experimental films. I’m currently working full-time creating branded content and social-media materials for Kyra TV, producing high-quality promotional videos and bringing new audience awareness to the YouTube platform. In my free time, I enjoy producing self-directed experimental videos alongside an avid interest in music and photography, leading to me shooting live events over the past few years -  including large festivals such as Sunfall (London), Dimensions &amp; Soundwave (Croatia). </p>
 
       <p>For my photography work, check out <a href="https://tmorsli.tumblr.com">tmorsli.tumblr.com</a></p>
 
