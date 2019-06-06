a11aa5daa3248fb3da86ed9f1d99f73eb96ba709
Jacob Thompson <thampsen@gmail.com>
2019-06-06 11:33:32 +0100

    update video and copyy

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <iframe className='bg-video di-ns dn' width="100%" height="100%" frameBorder="0" autoplay='true' src="https://www.youtube.com/embed/iUq2LTuj0H0?ecver=3&loop=1&playlist=iUq2LTuj0H0&autoplay=1&showinfo=0&mute=1&controls=0&vq=244" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <iframe className='reel-video dn-ns di' width="560" height="316" frameBorder="0" autoplay='true' src="https://www.youtube.com/embed/iUq2LTuj0H0?ecver=3&loop=1&playlist=iUq2LTuj0H0&showinfo=0&mute=1&controls=0&vq=244" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe className='bg-video di-ns dn' width="100%" height="100%" frameBorder="0" autoplay='true' src="https://www.youtube.com/embed/xrZfVjC9gQE?ecver=3&loop=1&playlist=iUq2LTuj0H0&autoplay=1&showinfo=0&mute=1&controls=0&vq=244" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    <iframe className='reel-video dn-ns di' width="560" height="316" frameBorder="0" autoplay='true' src="https://www.youtube.com/embed/xrZfVjC9gQE?ecver=3&loop=1&playlist=iUq2LTuj0H0&showinfo=0&mute=1&controls=0&vq=244" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </div>
 )
 
